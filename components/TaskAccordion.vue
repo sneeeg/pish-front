ecd62cbbcdf3de33f66ec817cab4e222fd57f566
Kuzmin Dmitriy <kuzmindmitriy24.03@gmail.com>
2021-05-19 17:32:50 +0700

    feat: Сверстать блок задач с адаптивом

---
+++ components/TaskAccordion.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="task-accordion">
+    <div
+      v-for="{ id, label, content } in tasks"
+      :key="id"
+      class="task-accordion__item"
+    >
+      <button
+        type="button"
+        class="task-accordion__label _visually-h5"
+        :class="{ _active: expanded.includes(id) }"
+        @click="expandToggle(id)"
+      >
+        {{ label }}
+      </button>
+
+      <TransitionExpand>
+        <div v-if="expanded.includes(id)" class="task-accordion__content">
+          <HTMLContent :html="content" />
+        </div>
+      </TransitionExpand>
+    </div>
+  </div>
+</template>
+<script>
+import TransitionExpand from '~/assets/js/utils/TransitionExpand'
+import HTMLContent from '~/components/Utils/HTMLContent'
+
+export default {
+  name: 'TaskAccordion',
+  components: {
+    TransitionExpand,
+    HTMLContent,
+  },
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    expanded: [],
+  }),
+  computed: {},
+  methods: {
+    expandToggle(id) {
+      if (this.expanded.includes(id)) {
+        const index = this.expanded.indexOf(id)
+        this.expanded.splice(index, 1)
+      } else {
+        this.expanded.push(id)
+      }
+    },
+    closeAll() {
+      this.expanded.splice(0, this.expanded.length)
+    },
+  },
+}
+</script>
+<style lang="scss">
+.task-accordion {
+  &__item {
+    border-bottom: 1px solid $color_grey_border;
+  }
+
+  &__label {
+    @include arrow-down(1.3rem, 3px, $color_red);
+    @include btn-reset;
+
+    position: relative;
+    display: block;
+    width: 100%;
+    padding: 2.4rem 0;
+    font-weight: bold;
+    text-align: left;
+  }
+
+  &__content {
+    &::after {
+      display: block;
+      height: 3.2rem;
+      content: '';
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <TaskAccordionBlock title="Задачи программы" img="/tasks/2.png" />
     <Section title="test">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci
       animi assumenda consequuntur, debitis magni maiores minima minus odio
@@ -16,10 +17,12 @@
 
 <script>
 import TextBlock from '~/components/TextBlock'
+import TaskAccordionBlock from '~/components/TaskAccordionBlock'
 
 export default {
   components: {
     TextBlock,
+    TaskAccordionBlock,
   },
 }
 </script>
