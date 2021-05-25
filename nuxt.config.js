7942e059cab3b3cee52f08bf319c6f254ce14ab7
Vladislav Belozerov <vladislavbelozerovtsk@gmail.com>
2021-05-25 12:50:05 +0700

    feat(): svg4everybody

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/svg4everybody.js',
     '~/plugins/injections.js',
     '~/plugins/directives.js',
     '~/plugins/server.js',
