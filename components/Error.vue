021a898e787f1b3d3eb6e49fbafcfec772767766
Dmitriy Kuzmin <kuzmindmitriy24.03@gmail.com>
2021-05-26 13:17:20 +0700

    feat: Реализовать локализацию компонента ошибки

--- components/Error.vue
+++ components/Error.vue
@@ -2,13 +2,15 @@
   <div class="error">
     <div class="error__content">
       <h1 class="error__title _visually-h2">
-        Иногда сайты тоже бывают перегруженными
+        {{ lang['error.title'] }}
       </h1>
       <p v-if="error.statusCode === 404" class="error__text">
-        Извините, страница не найдена. Попробуйте еще раз позже
+        {{ lang['error.404Text'] }}
       </p>
-      <p v-else class="error__text">произошла ошибка</p>
-      <Btn to="/" text="Вернуться на главную" is-link />
+      <p v-else class="error__text">
+        {{ lang['error.defaultText'] }}
+      </p>
+      <Btn to="/" :text="lang['error.toMain.text']" is-link />
     </div>
     <div class="error__image-wrapper">
       <img src="/error/404.png" />
@@ -17,6 +19,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Btn from '~/components/controls/Btn'
 export default {
   name: 'Error',
@@ -26,6 +29,9 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  computed: {
+    ...mapState('default', ['lang']),
   },
 }
 </script>
