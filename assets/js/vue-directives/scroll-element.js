d744144dde1f33707d96d69b3891b5801645e085
Vladislav Belozerov <vladislavbelozerovtsk@gmail.com>
2021-07-03 23:46:20 +0700

    fix(): remove animations from mobile

--- assets/js/vue-directives/scroll-element.js
+++ assets/js/vue-directives/scroll-element.js
@@ -1,9 +1,12 @@
 import gsap from 'gsap'
+import { isMobile } from 'mobile-device-detect'
 
 /**
  * Adds data att and styles
  */
 export default (el, binding) => {
+  if (isMobile) return
+
   const direction = binding.value || 'bottom'
   gsap.set(el, {
     willChange: 'opacity, transform',
